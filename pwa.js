219a46fb321508b787caefd347cbb0460d6b10fa
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2021-10-01 20:32:28 +0530

    Add files via upload

--- pwa.js
+++ pwa.js
@@ -1,16 +1,18 @@
 // op("head").insertAdjacentHTML("beforeend",`<link rel="manifest" href="pwa/manifest.json">`)
+let readyToDownload=false;
 if ('serviceWorker' in navigator) {
    window.addEventListener('load', function () { navigator.serviceWorker.register('sw.js').then(function (registration) { console.log('ServiceWorker registration successful with scope: ', registration.scope); }, function (err) { console.log('ServiceWorker registration failed: ', err); }); });
 }
 let deferredPrompt;
 downBtn1.style.display = "none"; 
 downBtn2.style.display = "none"; 
-log('came')
+
 window.addEventListener('beforeinstallprompt', (e) => {
-log('came here')
    e.preventDefault(); deferredPrompt = e;
    downBtn1.style.display = "";
    downBtn2.style.display = "";
+   readyToDownload=true;
+   
    downBtn.forEach(val=>{
       val.addEventListener('click', (e) => {
          deferredPrompt.prompt(); deferredPrompt.userChoice.then((choiceResult) => {
