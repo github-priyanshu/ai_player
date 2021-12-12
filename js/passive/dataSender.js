5013792c72eaf640feda28e5ee923c0c9e53e118
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2021-12-12 07:43:36 +0530

    Update dataSender.js

--- js/passive/dataSender.js
+++ js/passive/dataSender.js
@@ -1,4 +1,4 @@
-let shareName=["Team of fun","Awesome Code","Chahat","Telegram","Prashant","Swapnil","Akash","Josh","fb","insta","other"],
+let shareName=["Team of fun","Awesome Code","Chahat","Telegram","Prashant","Swapnil","Akash","Josh","fb","insta","other","qr code","person"],
 aiSharedBy,aiLoadedNum=localStorage.getItem("aiLoadedNum");
 
 setInterval(()=>{
