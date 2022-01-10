03a156aaa5d96faf60c0c8617e57dea688804bf3
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2022-01-10 21:13:41 +0530

    Add files via upload

--- js/passive/dataSender.js
+++ js/passive/dataSender.js
@@ -1,4 +1,4 @@
-let shareName=["Team of fun","Awesome Code","Chahat","Telegram","Prashant","Swapnil","Akash","Josh","fb","insta","other","qr code","person","code viewer site","ai kit"],
+let shareName=["Team of fun","Awesome Code","Chahat","Telegram","Prashant","Swapnil","Akash","Josh","fb","insta","other","qr code","person","code viewer site","ai kit","WISH"],
 aiSharedBy,aiLoadedNum=localStorage.getItem("aiLoadedNum");
 
 setInterval(()=>{
